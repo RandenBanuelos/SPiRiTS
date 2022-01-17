44bd13255ef81d98c99b746734566b214f749c65
Ray <jiahaolei99@163.com>
2022-01-17 02:02:21 -0800

    Ranged Combat added, Trigget event handler is still needed

--- SPiRiTS/Assets/Scripts/Player/PlayerCombat.cs
+++ SPiRiTS/Assets/Scripts/Player/PlayerCombat.cs
@@ -11,10 +11,35 @@ public class PlayerCombat : MonoBehaviour
     [SerializeField] private float attackRange = 0.5f;
     [SerializeField] private int attackDamage = 40;
 
+    private float attackForce = 15f;
+
+    public Rigidbody bullet;
+    public bool isRanged;
 
     // FUNCTIONS
 
     public void Attack()
+    {
+        if (!isRanged)
+        {
+            MeleeAttack();
+        }
+        else
+        {
+            RangedAttack();
+        }
+    }
+
+    private void RangedAttack()
+    {
+        Rigidbody bulletInstance =
+            Instantiate(bullet, attackPoint.position,attackPoint.rotation) as Rigidbody;
+
+        // Set the shell's velocity to the launch force in the fire position's forward direction.
+        bulletInstance.velocity = attackForce * attackPoint.forward;
+    }
+
+    private void MeleeAttack()
     {
         Collider[] hitEnemies = Physics.OverlapSphere(attackPoint.position, attackRange, enemyLayer);
 
